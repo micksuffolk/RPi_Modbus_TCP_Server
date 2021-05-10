353af37dea997b6cfc2867024f27ef6846a978d2
micksuffolk <82178824+micksuffolk@users.noreply.github.com>
2021-05-10 21:40:00 +0100

    Update LED_Function.py

--- LED_Function.py
+++ LED_Function.py
@@ -1,14 +1,20 @@
 from gpiozero import LED, Button
-#from time import sleep
 
-def LED_Function(GPIO_Number, event_wait):
+def LED_Function(GPIO_Number, event_wait, Program_Status_Code):
 
+    # Link LED Output to GPIO pin number...
     led = LED(GPIO_Number)
 
-    led.on()
-    event_wait.wait(0.5)
-    print("LED On")
+    # Pause before pulsing status LED...
+    event_wait.wait(2)
 
-    led.off()
-    event_wait.wait(0.5)
-    print("LED Off")
+    # Pulse LED the same amount of times as program status code...
+    for x in range(Program_Status_Code):
+
+        led.on()
+        event_wait.wait(0.4)
+        print("LED On")
+
+        led.off()
+        event_wait.wait(0.4)
+        print("LED Off")
